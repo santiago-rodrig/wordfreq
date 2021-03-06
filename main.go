d9b88e37e3e6becfe8f6bbae1da51dd8758dcb6e
santiago-rodrig <santo1996.29@gmail.com>
2021-03-06 17:47:35 -0400

    complete the program

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
 func main() {
-	println("not implemented")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	words := make(map[string]int)
+
+	for scanner.Scan() {
+		word := scanner.Text()
+
+		if unicode.IsPunct(rune(word[len(word)-1])) {
+			word = word[:len(word)-1]
+		}
+
+		words[word]++
+	}
+
+	for w, c := range words {
+		fmt.Printf("%s: %d\n", w, c)
+	}
 }
